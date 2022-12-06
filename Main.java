00f94db6db49c1a5fe369cb02e984399f7806759
Blaise Gozé <>
2022-12-06 19:45:55 +0000

    Made the code work.

--- Main.java
+++ Main.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
 class Main {
-{   
+  
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
       int quartsNeeded = 18;
       int gallonsNeeded;
       int extraQuartsNeeded;
+     
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
+     
+      while(true){
+        try {
+          System.out.print("Enter quarts needed >> ");
+          quartsNeeded = input.nextInt();
+        }
+          
+        catch(Exception e) {
+          System.out.println("Value must be an integer.");
+          input.nextLine();
+          continue;
+        }
+        break;
+      }
+
       gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
       extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
+     
       System.out.println("A job that needs " + quartsNeeded +
          " quarts requires " + gallonsNeeded + " gallons plus " +
          extraQuartsNeeded + " quarts.");
    }
+  
 }
